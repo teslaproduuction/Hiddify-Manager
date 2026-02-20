35485b3532bf82525f63fb86ef82be9f9b124272
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2026-02-21 02:28:26 +0330

    new: accountable mtproxy

---
+++ other/telegram/telemt/install.sh
@@ -0,0 +1,10 @@
+source /opt/hiddify-manager/common/package_manager.sh
+
+download_package telemt telemt.tar.gz
+if [ "$?" == "0"  ] || ! is_installed ./telemt; then
+    tar -xf telemt.tar.gz || exit 1
+    rm -rf telemt.tar.gz
+fi
+
+
+
