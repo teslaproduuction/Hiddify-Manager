461d9bde0cbf9b7a58d5122509ee9f05d2a41dc8
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2026-02-21 17:09:04 +0330

    fix: mtproxy

--- other/telegram/telemt/install.sh
+++ other/telegram/telemt/install.sh
@@ -4,6 +4,7 @@ download_package telemt telemt.tar.gz
 if [ "$?" == "0"  ] || ! is_installed ./telemt; then
     tar -xf telemt.tar.gz || exit 1
     rm -rf telemt.tar.gz
+    set_installed_version telemt
 fi
 
 
